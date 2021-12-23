4d12da1f4d6022ca284eed22bdd809f8b128afd6
iamstillcath <catherine_lucia@yahoo.com>
2021-12-23 18:01:10 +0100

    knight

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
-// immediate invoked function expression
-(function() {
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
-
-  //select both left and right button. Add event listeners
-  const buttons = document.querySelectorAll('.btn')
-  const imageDIV = document.querySelector('.img-container')
-  let counter = 0
-
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-      if (button.classList.contains('btn-left')){
-        counter--
-        if(counter < 0){
-          counter = pictures.length -1
+   (function() {
+    const pictures = [
+      "contBcg-0",
+      "contBcg-1",
+      "contBcg-2",
+      "contBcg-3",
+      "contBcg-4"
+    ];
+  
+    //select both left and right button. Add event listeners
+    const buttons = document.querySelectorAll('.btn')
+    const imageDIV = document.querySelector('#img-container')
+    let counter = 0
+  
+    buttons.forEach(function(button){
+      button.addEventListener('click', function(){
+        if (button.classList.contains('btn-left')){
+          counter--
+          if(counter < 0){
+            counter = parseInt(Math.random() *pictures.length)
+          }
+      
+          imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
         }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-      if (button.classList.contains('btn-right')){
-        counter++
-        if(counter > pictures.length -1){
-          counter = 0
+        if (button.classList.contains('btn-right')){
+          counter++
+          if(counter > 0 ){
+            counter = parseInt(Math.random() *pictures.length)
+          };
+          imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
         }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
+      })
     })
-  })
-})();
+  })();
