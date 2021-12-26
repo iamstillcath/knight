52b68dcd5f51df54e551b6a8ca251f443095bd13
iamstillcath <catherine_lucia@yahoo.com>
2021-12-26 22:47:34 +0100

    relish

--- js/app.js
+++ js/app.js
@@ -7,27 +7,30 @@
       "contBcg-4"
     ];
   
-    const buttons = document.querySelectorAll('.btn')
+    const button = document.querySelectorAll('.btn')
     const imageDIV = document.querySelector('#img-container')
-    let counter = 0
-  
-    buttons.forEach(function(button){
-      button.addEventListener('click', function(){
-        if (button.classList.contains('btn-left')){
+    let counter = parseInt(Math.random() *pictures.length)
+
+    imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+    button.forEach(function(button){
+      button.addEventListener('click' ,function(){
+        if (button.classList.entries('btn-left','btn-right')){
           counter--
-          if(counter < 0){
-            counter = parseInt(Math.random() *pictures.length)
+          if(counter < parseInt(Math.random() *pictures.length)){
+            // counter = parseInt(Math.random() *pictures.length)
           }
       
           imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
         }
-        if (button.classList.contains('btn-right')){
-          counter++
-          if(counter > 0 ){
-            counter = parseInt(Math.random() *pictures.length)
-          };
-          imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-        }
+
+        // img-container.style.backgroundImage
+        // if (button.classList.contains('btn-right')){
+        //   counter++
+        //   if(counter > 0 ){
+        //     counter = parseInt(Math.random() *pictures.length)
+        //   };
+        //   imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+        // }
       })
     })
   })();
